769897e474462ae2e6046fb58ea54b1985bc076d
JalilBNH <jalil.bnh13@gmail.com>
2024-11-17 23:34:33 +0100

    big update

--- tp2_imovi/tp2_Jalil.py
+++ tp2_imovi/tp2_Jalil.py
@@ -18,3 +18,21 @@ def make_projective(theta, t, ax, ay, x0, y0):
     
     P = K @ RT
     return P
+
+
+def projection(P, X):
+    X_h = np.vstack((X, np.ones((1, X.shape[1]))))
+    x_h = P @ X_h
+    
+    return x_h[:2, :] / x_h[2, :]
+
+cube_points = np.array([[0, 1, 1, 0, 0, 1, 1, 0],
+                        [0, 0, 1, 1, 0, 0, 1, 1],
+                        [0, 0, 0, 0, 1, 1, 1, 1]])
+
+P = make_projective(20, 2, 800, 800, 250, 250)
+projected_points = projection(P, cube_points)
+
+plt.figure()
+plt.scatter(projected_points[0, :], projected_points[1, :], c='blue', marker='o')
+plt.show()
